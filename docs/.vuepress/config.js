ce38550fd28265b4da7a271dcf7984cefd394d92
btea <2356281422@qq.com>
2021-08-03 17:15:51 +0800

    feat: add nav sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
     title: 'node api接口',
-    description: '接口说明文档'
+    description: '接口说明文档',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '导航', link: '/guide' }
+        ],
+        sidebar: [{ title: '默认', path: '' }]
+    }
 };
